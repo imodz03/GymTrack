44b23d16403d84ae945556cdc930efecd59fcb92
Elliot Brown <imodz03@gmail.com>
2018-02-27 16:34:42 +0000

    added html for registration form

--- Webapp/src/app/register/register.component.ts
+++ Webapp/src/app/register/register.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerUser = {
+    firstname: '',
+    surname: '',
+    username: '',
+    email: '',
+    height: 0,
+    weight: 0,
+    bodyfatPerc: 0,
+    password: ''
+
+  };
+
+  confPass: '';
+
+  triedConfPass = false;
+  passwordsMatch = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  signup(): void{
+    console.log(this.registerUser);
+  }
+
+  validate(): void{
+    if (this.registerUser.password !== this.confPass && this.triedConfPass){
+
+      this.passwordsMatch = false;
+
+    }else{
+
+      this.passwordsMatch = true;
+
+    }
+  }
+
+  tried(): void{
+    this.triedConfPass = true;
   }
 
 }
